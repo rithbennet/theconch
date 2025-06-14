3905551a3eee0bf7a2c998223cf89e0aee96caeb
Wan Zafirzan Bin Wan Tarmizan <wzafirzan@gmail.com>
2025-06-14 18:39:29 +0800

    Mic function and voice to text

--- frontend/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,12 @@ import Foundation
 
 import audioplayers_darwin
 import path_provider_foundation
+import record_darwin
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
